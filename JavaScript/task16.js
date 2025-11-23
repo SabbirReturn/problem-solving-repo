c2ed4cdbc964d247a07c732370b0ad81f1d36c4f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-23 23:06:42 +0600

    task16

--- JavaScript/task16.js
+++ JavaScript/task16.js
@@ -1,3 +1,16 @@
+// function checkLongestWord(sen){
+//     let words = sen.split(' ');
+//     let longest = '';
+//     for(let word of words){
+//         if(word.length > words.length){
+//             longest = word;
+//         }
+//     }
+//     return longest;
+// }
+// let sentence = 'I am learning Programming to become a programmer';
+// let checker = checkLongestWord(sentence);
+// console.log(checker)
 function checkLongestWord(sen){
     let words = sen.split(' ');
     let longest = '';
