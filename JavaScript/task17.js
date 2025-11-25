d3808b5cb814fbfa55fb0114490bb0562643c25e
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-25 23:35:43 +0600

    task

--- JavaScript/task17.js
+++ JavaScript/task17.js
@@ -1,6 +1,17 @@
 // Generate a random number between 10 to 20.
 
 
+// function randomNumber(num){
+//     let number = Math.round(Math.random() * num);
+//     console.log(number)
+
+
+// }
+// let number = 10
+// let random = randomNumber(number);
+// console.log(random)
+
+
 function randomNumber(num){
     let number = Math.round(Math.random() * num);
     console.log(number)
