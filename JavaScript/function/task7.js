467e96f0884f690d93dfa8485be97408dd912f9d
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-12-07 22:49:31 +0600

    again

--- JavaScript/function/task7.js
+++ JavaScript/function/task7.js
@@ -55,6 +55,25 @@
 
 // let name = checkName('Rafeee')
 // console.log(name);
+// function checkName(name){
+//     let goodLastChars = ['a','y','i','e','o','u','w']
+//     if(typeof name !== 'string'){
+//         return `Invalid`
+//     }
+//     name = name.toLowerCase()
+//     let lastChar = name[name.length-1]
+//     if( goodLastChars.includes(lastChar)){
+//         return ` Good Name`
+//     }
+//     else{
+//         return `Bad Name`
+//     }
+    
+    
+// }
+
+// let name = checkName('Rafeee')
+// console.log(name);
 function checkName(name){
     let goodLastChars = ['a','y','i','e','o','u','w']
     if(typeof name !== 'string'){
