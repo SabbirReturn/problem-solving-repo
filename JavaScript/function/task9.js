cafebf66d3889700a278ba0c8162cdadde93e525
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-08-06 23:37:03 +0600

    again

--- JavaScript/function/task9.js
+++ JavaScript/function/task9.js
@@ -6,9 +6,4 @@ function reverseString(sentence){
 let reverseSentence = reverseString('Sabbir Hasan');
 
 // console.log(reverseSentence)
-// console.log(reverseSentence)
-// console.log(reverseSentence)
-// console.log(reverseSentence)
-// console.log(reverseSentence)
-// console.log(reverseSentence)
 console.log(reverseSentence)
